a5694e88c3a7f2bc7759e7de828727baa9e5ec40
Shahzaib Khan <zsthedev>
2025-01-03 15:25:31 +0500

    cookies options updated

--- utils/sendToken.js
+++ utils/sendToken.js
@@ -2,10 +2,10 @@ export const sendToken = (res, user, message, statusCode = 200) => {
   const token = user.getJWTToken();
 
   const options = {
-    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000),
-    httpOnly: true,
-    sameSite: "none",
-    secure: true,
+    expires: new Date(Date.now() + 15 * 24 * 60 * 60 * 1000), // 15 days
+    httpOnly: true, // Prevent client-side JavaScript from accessing the cookie
+    sameSite: "lax", // Allow cookies for cross-site navigation while ensuring security
+    secure: false, // Allow cookies to work over HTTP
   };
   res.status(statusCode).cookie("token", token, options).json({
     success: true,
